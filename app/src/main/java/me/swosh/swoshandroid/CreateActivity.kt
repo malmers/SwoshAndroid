9674619ddbee32a8cd6f11471ccfd68c6507131d
Pontus Malm <malm@chalmers.it>
2017-07-22 01:02:43 +0200

    Button listener

--- app/src/main/java/me/swosh/swoshandroid/CreateActivity.kt
+++ app/src/main/java/me/swosh/swoshandroid/CreateActivity.kt
@@ -4,18 +4,23 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
 
 class CreateActivity : AppCompatActivity() {
 
-    lateinit var dropdown : Spinner
     lateinit var expiration : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
-        dropdown = findViewById(R.id.expiration)
+        val dropdown : Spinner = findViewById(R.id.expiration)
+        val phone_field : EditText = findViewById(R.id.phone)
+        val amount_field : EditText = findViewById(R.id.amount)
+        val message_field : EditText = findViewById(R.id.message)
+        val button : Button = findViewById(R.id.button)
 
         dropdown.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -24,6 +29,10 @@ class CreateActivity : AppCompatActivity() {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 expiration = resources.getStringArray(R.array.amount_value).get(position)
             }
+        }
+
+        button.setOnClickListener {
+            message_field.setText("Phone: ${phone_field.text}, amount: ${amount_field.text}, expiration: ${expiration}")
         }
     }
 }
