826de06e981c2ecd7af51bb2f7e5e97480ed1bc2
Pontus Malm <malm@chalmers.it>
2017-07-21 23:44:56 +0200

    Dropdown values

--- app/src/main/java/me/swosh/swoshandroid/CreateActivity.kt
+++ app/src/main/java/me/swosh/swoshandroid/CreateActivity.kt
@@ -2,11 +2,28 @@ package me.swosh.swoshandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
 
 class CreateActivity : AppCompatActivity() {
+
+    lateinit var dropdown : Spinner
+    lateinit var expiration : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        dropdown = findViewById(R.id.expiration)
+
+        dropdown.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                expiration = resources.getStringArray(R.array.amount_value).get(position)
+            }
+        }
     }
 }
