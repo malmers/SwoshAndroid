0b9e50f72551661380842c2f5c653c5b4a3aa052
Pontus Malm <malm@chalmers.it>
2017-07-25 18:26:17 +0200

    Rename layouts to fragments

--- app/src/main/java/me/swosh/android/CreateFragment.kt
+++ app/src/main/java/me/swosh/android/CreateFragment.kt
@@ -19,7 +19,7 @@ class CreateFragment : Fragment() {
     private lateinit var responseListener : ResponseListener
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.activity_create, container, false)
+        val view = inflater.inflate(R.layout.fragment_create, container, false)
 
         val dropdown : Spinner = view.findViewById(R.id.expiration)
         val phone_field : EditText = view.findViewById(R.id.phone)
